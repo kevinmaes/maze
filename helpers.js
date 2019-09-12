5e761d05b1b010c66e2cae27b3106f485e12bb70
Kevin Maes <kevin@tek212.com>
2019-09-11 21:15:03 -0400

    Comment out getNextSolutionCell

--- helpers.js
+++ helpers.js
@@ -36,36 +36,36 @@ const getNextCell = ({ pathId, grid, currentCell, stack }) => {
   }
 };
 
-const getNextSolutionCell = ({ pathId, grid, currentCell, stack }) => {
-  let next;
+// const getNextSolutionCell = ({ pathId, grid, currentCell, stack }) => {
+//   let next;
 
-  const neighbors = currentCell.getSolutionNeighbors(grid);
+//   const neighbors = currentCell.getSolutionNeighbors(grid);
 
-  // Pick random direction;
-  if (neighbors.length) {
-    next = pickNeighbor(neighbors);
-    return next.markSolution(currentCell, pathId);
-  } else {
-    // Dead end, must backtrack.
-    const prevCell = stack.pop();
+//   // Pick random direction;
+//   if (neighbors.length) {
+//     next = pickNeighbor(neighbors);
+//     return next.markSolution(currentCell, pathId);
+//   } else {
+//     // Dead end, must backtrack.
+//     const prevCell = stack.pop();
 
-    if (prevCell) {
-      prevCell.backtrack = true;
-      prevCell.unmarkSolution();
-      next = getNextCell({
-        grid,
-        currentCell: prevCell,
-        stack,
-      });
+//     if (prevCell) {
+//       prevCell.backtrack = true;
+//       prevCell.unmarkSolution();
+//       next = getNextCell({
+//         grid,
+//         currentCell: prevCell,
+//         stack,
+//       });
 
-      if (next === null) {
-        return next;
-      }
+//       if (next === null) {
+//         return next;
+//       }
 
-      return next.markSolution(prevCell, pathId);
-    } else {
-      // Search is complete.
-      return null;
-    }
-  }
-};
+//       return next.markSolution(prevCell, pathId);
+//     } else {
+//       // Search is complete.
+//       return null;
+//     }
+//   }
+// };
