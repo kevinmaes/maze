2d95ca4dabd016c1d15c584a3bbd50f90f167fb8
Kevin Maes <kevin@tek212.com>
2019-09-26 22:27:05 -0400

    Use Stage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Stage from './components/stage/Stage';
-import { P5Stage } from './components/p5Stage/P5Stage';
+import Stage from './components/stage/Stage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <P5Stage width={1000} height={1000} />
+      <Stage width={1000} height={1000} />
     </div>
   );
 };
