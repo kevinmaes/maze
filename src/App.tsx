e7d43d39f2a1af7238ccfb84b6ff3158a3959052
Kevin Maes <kevin@tek212.com>
2019-10-25 23:14:16 -0400

    Debounce state props

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
+import debounce from 'lodash/debounce';
+
 import logo from './logo.svg';
 import './App.css';
 import Stage from './components/stage/Stage';
+import useDebounce from '../src/components/hooks/useDebounce';
 
 const FPS_DEFAULT = 60;
 const CELL_SIZE_DEFAULT = 100;
 const BORDER_WEIGHT_DEFAULT = 1;
+
+const DEBOUNCE_MS = 500;
 
 const App: React.FC = () => {
   const [fps, setFPS] = React.useState(FPS_DEFAULT);
   const [cellSize, setCellSize] = React.useState(CELL_SIZE_DEFAULT);
   const [borderWeight, setBorderWeight] = React.useState(BORDER_WEIGHT_DEFAULT);
 
+  const debouncedFPS = useDebounce(fps, DEBOUNCE_MS);
+  const debouncedCellSize = useDebounce(cellSize, DEBOUNCE_MS);
+  const debouncedBorderWeight = useDebounce(borderWeight, DEBOUNCE_MS);
+
   return (
     <div className="App">
       <Stage
         width={500}
         height={500}
-        fps={fps}
-        cellSize={cellSize}
-        borderWeight={borderWeight}
+        fps={debouncedFPS}
+        cellSize={debouncedCellSize}
+        borderWeight={debouncedBorderWeight}
         pixelRatio={1}
       />
       <form>
