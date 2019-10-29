6ab7f36a3972d6548f3794f4ea59c39ea619efab
Kevin Maes <kevin@tek212.com>
2019-10-28 23:59:45 -0400

    Clean up some unused CSS props

--- src/components/stage/Stage.css.js
+++ src/components/stage/Stage.css.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
 
 export const Canvas = styled.canvas`
-  /* max-width: 1000px;
-  max-height: 800px; */
   border: 10px solid red;
-  /* transform: skew(40deg); */
-
-  /* visibility: hidden; */
 `;
