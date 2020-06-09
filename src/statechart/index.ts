f06e87989126120519024ce6cb6d178cad4c866d
Kevin Maes <kevin@tek212.com>
2020-06-09 13:44:26 -0400

    Install xstate and add statechart

---
+++ src/statechart/index.ts
@@ -0,0 +1,57 @@
+import { Machine } from 'xstate';
+
+const grid = new Grid();
+const startCell = grid.start();
+
+const machine = Machine(
+  {
+    id: 'maze-generation',
+    initial: 'initializing',
+    context: {
+      grid,
+      currentCell: startCell,
+      stack: [],
+    },
+    states: {
+      initializing: {
+        on: {
+          START: 'seeking',
+        },
+      },
+      seeking: {
+        onEntry: ['pushToStack', 'getNeighboringCells', 'pickNextCell'],
+        on: {
+          NEXT: [
+            {
+              cond: 'isDeadEnd',
+              target: 'backtracking',
+            },
+            { target: 'seeking' },
+          ],
+        },
+      },
+      backtracking: {
+        onEntry: ['popFromStack'],
+        on: {
+          '': [
+            {
+              cond: 'isAtStart',
+              target: 'complete',
+            },
+            {
+              target: 'seeking',
+            },
+          ],
+        },
+      },
+      complete: {},
+    },
+  },
+  {
+    guards: {
+      isDeadEnd: (ctx) => {
+        return false;
+      },
+    },
+  }
+);
