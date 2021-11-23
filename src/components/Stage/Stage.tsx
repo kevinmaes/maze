4d6a4acdfabf30df3881080858a8081c2a25a7ce
Kevin Maes <kevin@tek212.com>
2021-11-22 23:19:49 -0500

    Comment out array of blocked cells

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -54,7 +54,8 @@ const Stage = ({
         canvasCtx,
         cellSize,
         borderWeight,
-        blockedCells: [50, 54, 65, 80, 95, 110, 69, 84, 99, 114, 66, 68, 82],
+        // blockedCells: [50, 54, 65, 80, 95, 110, 69, 84, 99, 114, 66, 68, 82],
+        blockedCells: [],
       });
 
       send('INJECT_REFS', { gridRef, fps });
