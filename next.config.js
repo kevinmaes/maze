5b6b67d0b5917e662fccb492b7fb4498ca441693
Kevin Maes <kevin@tek212.com>
2022-04-01 22:22:01 -0400

    Delete commented next.config.js code

--- next.config.js
+++ next.config.js
@@ -17,25 +17,3 @@ module.exports = {
   images: {
     disableStaticImages: true,
   },
-
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   // Important: return the modified config
-
-  // return config;
-  //   return {
-  //     ...config,
-  //     // plugins: [
-  //     //   new webpack.DefinePlugin({
-  //     //     VERSION: '1.2.3', //JSON.stringify(require('./package.json').version),
-  //     //   }),
-  //     // ],
-  //     // module: {
-  //     //   plugins: [
-  //     //     new webpack.DefinePlugin({
-  //     //       VERSION: JSON.stringify(require('./package.json').version),
-  //     //     }),
-  //     //   ],
-  //     // },
-  //   };
-  // },
-};
