252a0814f72edd6e6d03bda62c00c255fd88ea15
Kevin Maes <kevin@tek212.com>
2022-04-01 20:27:59 -0400

    Delete .next from repo (will be ignored soon)

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/generation":["static\u002Fchunks\u002Fpages\u002Fgeneration.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgeneration"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
