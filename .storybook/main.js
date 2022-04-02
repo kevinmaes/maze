6715dd09a1b164a9e32340f39c57132352d83621
Kevin Maes <kevin@tek212.com>
2022-04-02 16:13:28 -0400

    Abort storybook fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,51 @@
+// module.exports = {
+//   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+//   addons: [
+//     '@storybook/addon-actions',
+//     '@storybook/addon-links',
+//     '@storybook/addon-essentials',
+//     '@storybook/preset-create-react-app',
+//     'storybook-addon-next-router',
+//   ],
+//   // framework: '@storybook/react',
+//   // core: {
+//   //   builder: 'webpack5',
+//   // },
+//   webpackFinal: (config) => {
+//     const fileLoaderRule = config.module.rules.find(
+//       (rule) => rule.test && rule.test.test('.svg')
+//     );
+//     fileLoaderRule.exclude = /\.svg$/;
+
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       enforce: 'pre',
+//       loader: require.resolve('@svgr/webpack'),
+//     });
+
+//     return config;
+//   },
+// };
+
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../**/**/*.stories.mdx', '../**/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
+    'storybook-addon-next-router',
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+  webpackFinal: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test('.svg')
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
+    });
+
+    return config;
   },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,69 @@
-import '../src/index.css';
+// import '../src/index.css';
+
+// import GlobalStyle from '../src/styles/GlobalStyle';
+
+// export const parameters = {
+//   actions: { argTypesRegex: '^on[A-Z].*' },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// };
+
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+// import { initializeWorker, mswDecorator } from 'msw-storybook-addon';
+// import { ThemeProvider } from 'styled-components';
+import * as NextImage from 'next/image';
+import GlobalStyle from '../src/styles/GlobalStyles';
+// import theme from '../src/styles/theme';
+// import { useStore } from '../src/redux/store';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+// Init MSW
+// https://storybook.js.org/addons/msw-storybook-addon
+// initializeWorker();
+// addDecorator(mswDecorator);
+
+// Fix for NextJS Image component to render in storybook
+// https://github.com/vercel/next.js/issues/18393#issuecomment-783269086
+// const OriginalNextImage = NextImage.default;
+// Object.defineProperty(NextImage, 'default', {
+//   configurable: true,
+//   value: (props) => <OriginalNextImage {...props} unoptimized />,
+// });
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: '^(on|handle)[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  options: {
+    storySort: {
+      order: ['Style Guide', 'UI Components', 'Components', 'Pages'],
+    },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
 };
+
+export const decorators = [
+  (Story, ctx) => {
+    // const store = useStore(
+    //   ctx.parameters ? ctx.parameters.initialReduxState : undefined
+    // );
+
+    return (
+      <>
+        <GlobalStyle />
+        <Story />
+      </>
+    );
+  },
+];
