1b13c0677f104bd5b9daf8dbeafe480eeafdeb73
Kevin Maes <kevin@tek212.com>
2022-04-03 14:57:16 -0400

    Comment out 2 component stories

--- src/components/Controls/Controls.stories.tsx
+++ src/components/Controls/Controls.stories.tsx
@@ -1,58 +1,58 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Controls } from './Controls';
-import { AppMachineEventId } from '../../statechart/appMachineTypes';
+// import { Controls } from './Controls';
+// import { AppMachineEventId } from '../../statechart/appMachineTypes';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'Controls',
-  component: Controls,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Controls>;
+// // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// export default {
+//   title: 'Controls',
+//   component: Controls,
+//   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//   },
+// } as ComponentMeta<typeof Controls>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Controls> = (args) => (
-  <Controls {...args} />
-);
+// // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// const Template: ComponentStory<typeof Controls> = (args) => (
+//   <Controls {...args} />
+// );
 
-export const Idle = Template.bind({});
-Idle.args = {
-  state: {
-    can: (arg: AppMachineEventId) => false,
-  },
-};
+// export const Idle = Template.bind({});
+// Idle.args = {
+//   state: {
+//     can: (arg: AppMachineEventId) => false,
+//   },
+// };
 
-export const Initializing = Template.bind({});
-Initializing.args = {
-  state: {
-    can: (arg: AppMachineEventId) => arg === AppMachineEventId.PLAY,
-  },
-};
+// export const Initializing = Template.bind({});
+// Initializing.args = {
+//   state: {
+//     can: (arg: AppMachineEventId) => arg === AppMachineEventId.PLAY,
+//   },
+// };
 
-export const Playing = Template.bind({});
-Playing.args = {
-  state: {
-    can: (arg: AppMachineEventId) =>
-      arg === AppMachineEventId.PAUSE || arg === AppMachineEventId.STOP,
-  },
-};
+// export const Playing = Template.bind({});
+// Playing.args = {
+//   state: {
+//     can: (arg: AppMachineEventId) =>
+//       arg === AppMachineEventId.PAUSE || arg === AppMachineEventId.STOP,
+//   },
+// };
 
-export const Paused = Template.bind({});
-Paused.args = {
-  state: {
-    can: (arg: AppMachineEventId) =>
-      arg === AppMachineEventId.PLAY ||
-      arg === AppMachineEventId.STOP ||
-      arg === AppMachineEventId.STEP_FORWARD,
-  },
-};
+// export const Paused = Template.bind({});
+// Paused.args = {
+//   state: {
+//     can: (arg: AppMachineEventId) =>
+//       arg === AppMachineEventId.PLAY ||
+//       arg === AppMachineEventId.STOP ||
+//       arg === AppMachineEventId.STEP_FORWARD,
+//   },
+// };
 
-export const Done = Template.bind({});
-Done.args = {
-  state: {
-    can: (arg: AppMachineEventId) => arg === AppMachineEventId.START_OVER,
-  },
-};
+// export const Done = Template.bind({});
+// Done.args = {
+//   state: {
+//     can: (arg: AppMachineEventId) => arg === AppMachineEventId.START_OVER,
+//   },
+// };
---
+++ src/components/Levers/Levers.stories.tsx
@@ -0,0 +1,31 @@
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+
+// import { Levers } from './Levers';
+// import { defaultGenerationParams } from '../../statechart/appMachine';
+
+// // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// export default {
+//   title: 'Levers',
+//   component: Levers,
+//   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//   },
+// } as ComponentMeta<typeof Levers>;
+
+// // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// const Template: ComponentStory<typeof Levers> = (args) => <Levers {...args} />;
+
+// export const Enabled = Template.bind({});
+// Enabled.args = {
+//   enabled: true,
+//   params: defaultGenerationParams,
+//   updateFromLevers: action('changed'),
+// };
+
+// export const Disabled = Template.bind({});
+// Disabled.args = {
+//   enabled: false,
+//   params: defaultGenerationParams,
+// };
