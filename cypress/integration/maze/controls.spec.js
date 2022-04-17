732a78ec000f8a2f9532d514cc57b84a4ae38265
Kevin Maes <kevin@kevinmaes.com>
2022-04-17 18:30:37 -0400

    Issue 444 - Install Cypress (#445)
    * Install cypress
    
    * Add example spec files and cypess config
    
    * Add initial test files and test Controls.tsx
    
    * Add more tests for Controls
    
    * Add test for Levers
    
    * Add example files and videos
    
    * Add more tests for Levers
    
    * Add projectId to upload test results
    
    * Add data-test-id to Levers Fieldset
    
    * Delete videos from remote
    
    * Delete videos from remote
    
    * Add cypress videos and screenshots to .gitignore
    
    * Bump patch version
    
    * Add base url to config

---
+++ cypress/integration/maze/controls.spec.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+
+const WAIT1 = 200;
+const ONE_SECOND = 1000;
+const MIN_GENERATION_WAIT = 8 * ONE_SECOND;
+
+context('Controls', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+  it('should play to start', () => {
+    cy.get('#PLAY').click();
+
+    cy.get('#PLAY').should('not.exist');
+    cy.get('#PAUSE').should('be.enabled');
+    cy.get('#STOP').should('be.enabled');
+  });
+
+  it('should play to start and be able to stop', () => {
+    cy.get('#PLAY').click();
+    cy.wait(WAIT1);
+
+    cy.get('#STOP').click();
+
+    cy.get('#STOP').should('be.disabled');
+    cy.get('#PLAY').should('be.enabled');
+  });
+
+  it('should play to start and toggle beteween pause and play again', () => {
+    cy.get('#PLAY').click();
+    cy.get('#PLAY').should('not.exist');
+
+    cy.wait(WAIT1).get('#PAUSE').click();
+    cy.get('#PAUSE').should('not.exist');
+
+    cy.wait(WAIT1).get('#PLAY').click();
+    cy.get('#PLAY').should('not.exist');
+
+    cy.wait(WAIT1).get('#PAUSE').click();
+    cy.get('#PAUSE').should('not.exist');
+    cy.get('#PLAY').should('not.be.disabled');
+  });
+
+  it('should play to start and be able to start over after generation is done', () => {
+    cy.get('#PLAY').click();
+    cy.wait(MIN_GENERATION_WAIT);
+
+    cy.get('#PLAY').should('be.disabled');
+    cy.get('#START_OVER').should('not.be.disabled');
+  });
+});
---
+++ cypress/integration/maze/levers.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+const WAIT1 = 200;
+const ONE_SECOND = 1000;
+const MIN_GENERATION_WAIT = 8 * ONE_SECOND;
+
+context('Levers', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+  it('should be enabled by default', () => {
+    cy.get('[data-test-id="levers-fieldset"]').should('be.enabled');
+  });
+
+  it('should be disabled once maze generation is plqying', () => {
+    cy.get('#PLAY').click();
+
+    cy.get('[data-test-id="levers-fieldset"]').should('be.disabled');
+  });
+
+  it('should be re-enabled once maze generation is done and reset', () => {
+    cy.get('#PLAY').click();
+    cy.wait(8000);
+    cy.get('#START_OVER').click();
+
+    cy.get('[data-test-id="levers-fieldset"]').should('be.enabled');
+  });
+});
