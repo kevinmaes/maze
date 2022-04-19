10778ef603644c669735a1a129e8ed80293ec2ed
Kevin Maes <kevin@tek212.com>
2022-04-18 22:26:15 -0400

    Add loading of sound files to next.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,32 @@
 module.exports = {
-  webpack(config, { webpack }) {
+  webpack(config, { webpack, isServer }) {
+    // Support for .svg
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
 
+    // Support for .wav
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve('url-loader'),
+          options: {
+            limit: config.inlineImageLimit,
+            fallback: require.resolve('file-loader'),
+            publicPath: `${config.assetPrefix}/_next/static/images/`,
+            outputPath: `${isServer ? '../' : ''}static/images/`,
+            name: '[name]-[hash].[ext]',
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    });
+
+    // Support for loading package.json contents into the app.
     config.plugins.push(
       new webpack.DefinePlugin({
         VERSION: JSON.stringify(require('./package.json').version),
