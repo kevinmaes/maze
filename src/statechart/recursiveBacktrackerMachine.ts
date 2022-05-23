8ade6a7d3e197d7bfb0d4c83ad485d9514695ff8
Kevin Maes <kevin@kevinmaes.com>
2022-05-22 22:03:05 -0400

    Delete initial context for maze generation machine

--- src/statechart/recursiveBacktrackerMachine.ts
+++ src/statechart/recursiveBacktrackerMachine.ts
@@ -9,17 +9,6 @@ import type { IGrid } from '../components/generation/Grid';
 import { seek } from '../components/generation/seek';
 import { ICell } from '../components/generation/Cell';
 
-const initialRecursiveBacktrackerMachineContext: MazeGenerationContext = {
-  canPlay: false,
-  currentCell: undefined,
-  eligibleNeighbors: [],
-  fps: 3,
-  grid: undefined,
-  pathId: 'abc',
-  stack: [],
-  startIndex: 0,
-};
-
 export const generationAlgorithmMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QFsCGAvMBaGA7MATqgC4CWA9rgHSzGoHEDEAHrSWFagGbGFUDKAUUEBpAPoBJAHIAVQQCUAagEEAMolAAHcrFJlKGkM0QA2ACwmqARgAMADgDMdgEwBWG1bNWrJgDQgAT0QsBwBOOyo7M2dQr2dzBw8TAF9k-zRMHDB8In1qWDAwAGtGQ21dPMNjBDMHVyobJytXUNCfVwB2GI7-IIQsGIcqZ2c7Oy6TZzMbMzsbV1T0jGw8QhIKalQIADdUXABjMFKkEHK9DargjtmqaJGO1zNrq0TQ3sRQqlcTNucbUIc3lirUWIAyK2yazynB2e0OLDYvE4PD4QlEklkChU6hOZ0qJ2qVgBVBMdlCzm811GoVco3e-ReHSoHVCpP+JhMVgpzQ6oPBWRy60oVAARqh9kViEQJcctDpzgYCcFvMMHqEOg4zG0oh0OiZXPTmvUHLqXPYHM4wqEZnzlgKoRsqPtyMhNAAbMC8RjyQT8GTKeQyMry-GgarW251TyjGZWcb6+m0hrki0xGymhyTBZpMF21a5DaMaQAKUEAGEZGIfQAxfjBioXJX9PX1OMdZoWRwPWn0qY2EkWTkcqzPTopUG4cgQOCGfn5oXUUgQD31hW4S7NqbMmwmdk2EYzaL0gb6qjhIHXTkW6YOW2ZefQtgMVehozBMJmBquOMteYsurOMe0TGnYP6PLS8RhHeEKCo+hRFC+jZhogLSfNcryZiYmodHYBqBMqjS3L80zuFSP7QfaBbClsuwHGAiGKshDKnpaWrOO2Pzfl0djHrqVBOLM9xmFqszZks96QlR1BihKUrighuIhkhb79GE9SzI0nItC0tQPL2tTDOadgcrUXjmLyOZzpJC5Oi67qevRikNoxKnTJ+jz7hybjst+vb-JE9jRi0cZeWYFEPspeLKdUWA4Z8bYdqSJquD2+H9N+-ZRo0mrdG4qSpEAA */
   createMachine({
@@ -28,7 +17,6 @@ export const generationAlgorithmMachine =
       events: {} as MazeGenerationEvent,
     },
     tsTypes: {} as import('./recursiveBacktrackerMachine.typegen').Typegen0,
-    context: initialRecursiveBacktrackerMachineContext,
     id: 'generationAlgorithmMachine',
     initial: 'maze-idle',
     states: {
