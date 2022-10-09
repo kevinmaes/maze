984a285117159b101267cb5fa73ff2cfe5a57ef3
Kevin Maes <kevin@kevinmaes.com>
2022-10-08 22:45:11 -0400

    Add delays to options object

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -58,6 +58,7 @@ const App = () => {
       // Can switch between algorithm machines by making this a function
       childMachine: generationAlgorithmMachine,
     },
+    delays: {},
   });
 
   const [leversAreChanging, setLeversAreChanging] = useState(false);
