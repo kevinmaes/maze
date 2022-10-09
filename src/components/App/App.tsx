216e0ce434522d37feefd56b372c70a5d51d566e
Kevin Maes <kevin@kevinmaes.com>
2022-10-08 22:46:49 -0400

    Add guards to options object

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -58,6 +58,7 @@ const App = () => {
       // Can switch between algorithm machines by making this a function
       childMachine: generationAlgorithmMachine,
     },
+    guards: {},
     delays: {},
   });
 
