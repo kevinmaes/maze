659b3470ad93c67e5b88fee78a538f50d164c79a
Kevin Maes <kevin@kevinmaes.com>
2023-09-08 22:47:21 +0200

    Work on fixing type errors. WIP

--- src/statechart/app.machine.ts
+++ src/statechart/app.machine.ts
@@ -33,40 +33,41 @@ const initialAppMachineContext: AppMachineContext = {
 
 export const appMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QEMAOqDEMB2YBOyALgJYD22AdKsgQLYWxiEDaADALqKiqmzEnkuIAB6IAtAE4ATBIoSAHAGZWU+QDYA7Gplr5AGhABPcQBYJrCgEZ50xRICsa1pZP2HAX3cG0qCgEkIABswDDwwADNYCmJsACswAGMWDiEePgFsIVEEMQ0TCykTeVZFJzc1ZSkDYxzFe0sKVXtFE1VLR3qNe09vdAoAcTBcAhJsKAwIYlhUQORDCgBXVAgiMDZOJBA0-jJMzez5KQ1G+w1WIqk61i7q8Qk1CkUtdQlS+XlLKXsTHpAfAaG+CIMXGOCBGQo4RiUwAFutUrwdoJ9ognIoKHlFJ9LPd6vl7LccporEoZE8ZBJ2q1fv9BsNgWN-NgdshAsQAF4gjAJciEPCkQJRGZzeGbbYZLLiRSFRrqLR1NSWVgVEyEsSXCzKLquSylLTNGl9OngkEUAAKs0MXJ52D5AqFyAWjFF3EREpRCEu8goNnaxSelicrHs+iM4i+snaCg+wY0l3aikNvmNI1NFrm1t5-MFDEIpFQLq2bt2kpyGnRNg0SnMEar5bVLhMFCKTg+Wle10OSYB9NGUHNjsYEG5WftVEthfFJY9hSbrW+dm0rkqGjVdQaTRabQ6li63ZTDP7ZsHkBHtuzUVgeYLKTFxeRoGyuQrEirdhUbjrijVJge7UDgalLu7wSD8Xh-EagKpoyx5OqeNp2jmV5gL44SkHgADuNAQJO957I+qK-o8oHKtolzSholhrnYjRPNIyrSI40rdOB-wACLkCE-xhFeNDJBsrrpNOBE5K8Dzlsoai6JcUhqA4hLtM2ujyGcaghsUnSJr82CkBAcBCD4CJCQ+Ih3PITYSaRhzSnJBJhjkbhSI0JjtLojgVLo3YBMERlIvhpk5OoFifnUNi6OcZhrqojxYmiahmHYUj7lBh6+e6IliM03pKqcXRYuZSirvZmXnBiFSfBUXTmGYajJb2pp+MyJCshyIJpcJAViGYFg5RoeXWEUpRqhqjzXN89R6mpWm9MmKV9ualptXexn+U+nzopYLh9dKyisMURzDc0GISNIr5aJ8fUqHVJowSeEDtSZ2SOLI5UOM4qiycNpSNMqLjWOYr67rVrF9BxuAPatqL2D1hxxlIUiBqdVT2Yo3qtLJrAKF8ypmGBnhAA */
-  createMachine({
-    types: {
-      context: {} as AppMachineContext,
-      events: {} as AppMachineEvent,
-    },
-    context: initialAppMachineContext,
-    id: 'app',
-    initial: 'Idle',
-    states: {
-      Idle: {
-        on: {
-          'refs.inject': {
-            actions: ['storeGridRef'],
-            target: 'Generating',
+  createMachine(
+    {
+      types: {
+        context: {} as AppMachineContext,
+        events: {} as AppMachineEvent,
+      },
+      context: initialAppMachineContext,
+      id: 'app',
+      initial: 'Idle',
+      states: {
+        Idle: {
+          on: {
+            'refs.inject': {
+              actions: ['storeGridRef'],
+              target: 'Generating',
+            },
           },
         },
-      },
-      Generating: {
-        initial: 'Initializing',
-        invoke: {
-          id: 'generationAlgorithmMachine',
-          // src: 'childMachine',
-          src: generationAlgorithmMachine,
-          input: ({ context }) => {
-            const defaultChildMachineContext = {
-              canPlay: true,
-              currentCell: undefined,
-              eligibleNeighbors: [],
-              stack: [],
-              startIndex: 0,
-            };
+        Generating: {
+          initial: 'Initializing',
+          invoke: {
+            id: 'generationAlgorithmMachine',
+            src: generationAlgorithmMachine,
+            input: ({ context }) => {
+              const defaultChildMachineContext = {
+                canPlay: true,
+                currentCell: undefined,
+                eligibleNeighbors: [],
+                stack: [],
+                startIndex: 0,
+              };
 
-            const childContext: ContextFrom<typeof generationAlgorithmMachine> =
-              {
+              const childContext: ContextFrom<
+                typeof generationAlgorithmMachine
+              > = {
                 ...defaultChildMachineContext,
                 fps: context.generationParams.fps,
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -74,75 +75,75 @@ export const appMachine =
                 pathId: context.generationSessionId.toString(),
               };
 
-            return childContext;
+              return childContext;
+            },
           },
-        },
-        on: {
-          'display.update': {
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
-            actions: [() => {}],
+          on: {
+            'display.update': {
+              // eslint-disable-next-line @typescript-eslint/no-empty-function
+              actions: [() => {}],
+            },
+            'generation.finish': {
+              target: 'Done',
+            },
           },
-          'generation.finish': {
-            target: 'Done',
-          },
-        },
-        states: {
-          Initializing: {
-            on: {
-              'controls.play': {
-                target: 'Playing',
+          states: {
+            Initializing: {
+              on: {
+                'controls.play': {
+                  target: 'Playing',
+                },
+              },
+            },
+            Playing: {
+              entry: 'startGenerationAlgorithmMachine',
+              on: {
+                'controls.pause': {
+                  actions: ['pauseGenerationAlgorithmMachine'],
+                  target: 'Paused',
+                },
+                'controls.stop': {
+                  actions: ['refreshGenerationSessionId'],
+                  target: '#app.Idle',
+                },
+              },
+            },
+            Paused: {
+              on: {
+                'controls.play': {
+                  actions: ['playGenerationAlgorithmMachine'],
+                  target: 'Playing',
+                },
+                'controls.stop': {
+                  actions: ['refreshGenerationSessionId'],
+                  target: '#app.Idle',
+                },
+                'controls.step.forward': {
+                  actions: ['stepGenerationAlgorithmMachine'],
+                },
               },
             },
           },
-          Playing: {
-            entry: 'startGenerationAlgorithmMachine',
-            on: {
-              'controls.pause': {
-                actions: ['pauseGenerationAlgorithmMachine'],
-                target: 'Paused',
-              },
-              'controls.stop': {
-                actions: ['refreshGenerationSessionId'],
-                target: '#app.Idle',
-              },
-            },
-          },
-          Paused: {
-            on: {
-              'controls.play': {
-                actions: ['playGenerationAlgorithmMachine'],
-                target: 'Playing',
-              },
-              'controls.stop': {
-                actions: ['refreshGenerationSessionId'],
-                target: '#app.Idle',
-              },
-              'controls.step.forward': {
-                actions: ['stepGenerationAlgorithmMachine'],
-              },
+        },
+        Done: {
+          on: {
+            'app.restart': {
+              actions: ['refreshGenerationSessionId'],
+              target: 'Idle',
             },
           },
         },
       },
-      Done: {
-        on: {
-          'app.restart': {
-            actions: ['refreshGenerationSessionId'],
-            target: 'Idle',
-          },
+      on: {
+        'generation.param.set': {
+          actions: ['updateGenerationParams'],
+          target: 'Idle',
         },
       },
     },
-    on: {
-      'generation.param.set': {
-        actions: ['updateGenerationParams'],
-        target: 'Idle',
+    {
+      actors: {
+        generationAlgorithmMachine,
       },
-    },
-  });
-
-// const service = interpret(appMachine).subscribe((state) => {
-//   console.log('appMachine:', state.value);
-// });
-
-// service.start();
+    }
+  );
--- src/statechart/recursiveBacktracker.machine.ts
+++ src/statechart/recursiveBacktracker.machine.ts
@@ -2,203 +2,154 @@ import { assign, createMachine, sendParent } from 'xstate';
 
 import type { IGrid } from '../components/generation/Grid';
 
-import { Ref } from 'react';
+// import { Ref } from 'react';
 import { ICell } from '../components/generation/Cell';
 import { seek } from '../components/generation/seek';
 
 export const generationAlgorithmMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QwHZgE4EMAuBLA9igIIA2U+6u2AFgLYCymAxtbmgMTpgBmsAdGwBWYJtgDaABgC6iUAAd8sKgRSyQAD0QBaACwBWAIx8DEgwHY9OgJwAOCRJtmJOgDQgAntoBMXq8as6OubWTs42egC+EW6oGDgqpOSUNAzMrBxMhNjo+CT8ciSY7pIySCAKSniEapoIBnp8el4AbFZWZjY6Zs06zQDM4W6eCFoGXjp8jl42A3rtc4aR0SCxWFXEZBRUdIwsbGDsmSjZufmYAK6wYCVqFcrVZbUtXpM6NjYGAwMmVkOIgWY+H19DZxsCBmZAlEYmA0GsEptkjs0vtDlkcnk+LBsGA5HxuBQAO6YdAQG5lO7rGqIVqTMx9KzNRx6GZ9Hp6P4jAyMxo6MJBWw6Fp6JYwuHxQiJLYpXbpMB8ADisLi6wABABJCAkA6rCUoLHYEniaS3RT3VSPbRdPqNZpmLz1Lr6B0cjzafqNcbTfrNcYmPps6ErZXwyWI7apPZoPjqlDKTAkXAALzYUHY6mxOHlmG4OPQAAoAMoAUWLAGkAPrqgByABViwAlABqRAAMgBKdi69ZSpGRuUxuN4BPJ1Pk+RmqmWkZee18FoOPoSDpeCFszndIHc5oGfRNTpNIPdhFJCOy-Z8QtgMAAa1T7HH5UnKmpCCXEle72sgW5elMzQ3Zot0Zcxuj-HQ+iPEM9V7M8UWjIgIAAN0wFAmHvR9KRfad+gaKwDDtJdgWdawN0BZdej3NkJAZAioPFHtwxleD5UQlC0PvDNDRxPgczzItS0rGt62bNtO2PMNT2YqNWOQ1D0JQKBMOfB5QCeAwbGMTozHtIVAhsAJXDdEYA0mZdmgkCygh0-QrHolUT2lZEZL4AAhZgb2yDyMJNCkVItNTEHCG0HB08ZWhMSwzE5AjNIsmwLLGP8+jMEwdHs0MNik5yB3cphPKwfKMIMUoJ0qbDAoQboXisCQ9A6RkPlBADjJdPh7VShLrASiwhSiZYUHwCA4DUCSsqc-t9lNcrVI0bRLA-AiFyCOZ6TaTktD6Ai+Dtb0XU6T47OWMbYOkgclQYlQNS1MBpvNV9dCZPgJEZZp+isaYGSCDbdxtN6LHaKxDAIiwbAymCmJyi9Y3jRMU0Uu6p0q3QPuMX0rPqdoGV+VqGnBRkPu6fprOacHGOyyboyvW9U0Riq5qqj8mR0j5TAFFkvE5AYgRSt6DKSmiAjJxy+3PBC5I4hG-JmgKGaFIxLH0CwBmtcEuc0gMif5rxkqF47oPJiaxflPKCu8qWyvu6cDr4Nppg+SEujtzlDB5z56T0AN9D6cZRWDS7JKNli+AAMTYXBYGoSA6dm2pHA-T2uicAZ+hmGxOWmPH6WcdpZz6EVSf6oA */
-  createMachine({
-    types: {
-      // typegen: {},
-      context: {} as {
-        canPlay: boolean;
-        currentCell: ICell | undefined;
-        eligibleNeighbors: ICell[];
-        fps: number;
-        grid: IGrid | undefined;
-        pathId: string;
-        stack: ICell[];
-        startIndex: number;
-      },
-      events: {} as
-        | {
-            type: 'refs.inject';
-            params: { gridRef: Ref<IGrid> };
-          }
-        | {
-            type: 'generation.start';
-          }
-        | {
-            type: 'controls.play';
-          }
-        | {
-            type: 'controls.pause';
-          }
-        | {
-            type: 'controls.step.forward';
-          }
-        | {
-            type: 'display.update';
-          },
-    },
-    id: 'generationAlgorithmMachine',
-    context: ({ input }) => input,
-    initial: 'Generation Idle',
-    states: {
-      'Generation Idle': {
-        on: {
-          'generation.start': {
-            target: 'Initializing',
-          },
-        },
-      },
-      Initializing: {
-        entry: ['initGeneration', 'visitStartCell', 'pushToStack'],
-        after: {
-          SEEK_INTERVAL: {
-            guard: 'playing is allowed',
-            target: 'Seeking',
+  createMachine(
+    {
+      // types: {
+      //   context: {} as {
+      //     canPlay: boolean;
+      //     currentCell: ICell | undefined;
+      //     eligibleNeighbors: ICell[];
+      //     fps: number;
+      //     grid: IGrid | undefined;
+      //     pathId: string;
+      //     stack: ICell[];
+      //     startIndex: number;
+      //   },
+      //   events: {} as
+      //     | {
+      //         type: 'refs.inject';
+      //         params: { gridRef: Ref<IGrid> };
+      //       }
+      //     | {
+      //         type: 'generation.start';
+      //       }
+      //     | {
+      //         type: 'controls.play';
+      //       }
+      //     | {
+      //         type: 'controls.pause';
+      //       }
+      //     | {
+      //         type: 'controls.step.forward';
+      //       }
+      //     | {
+      //         type: 'display.update';
+      //       },
+      // },
+      id: 'generationAlgorithmMachine',
+      // context: ({ input }) => input,
+      initial: 'Generation Idle',
+      states: {
+        'Generation Idle': {
+          on: {
+            'generation.start': {
+              target: 'Initializing',
+            },
           },
         },
-      },
-      Seeking: {
-        entry: ['findNeighbors', sendParent({ type: 'display.update' })],
-        always: {
-          target: 'Advancing',
-        },
-      },
-      Advancing: {
-        entry: ['pickNextCell', 'pushToStack'],
-        after: {
-          SEEK_INTERVAL: {
-            guard: 'playing is allowed',
-            target: 'Seeking',
+        Initializing: {
+          entry: ['initGeneration', 'visitStartCell', 'pushToStack'],
+          after: {
+            SEEK_INTERVAL: {
+              guard: 'playing is allowed',
+              target: 'Seeking',
+            },
           },
         },
-        always: {
-          guard: 'reached a dead end',
-          target: 'Backtracking',
+        Seeking: {
+          entry: ['findNeighbors', sendParent({ type: 'display.update' })],
+          always: {
+            target: 'Advancing',
+          },
         },
-      },
-      Backtracking: {
-        entry: 'popFromStack',
-        always: [
-          {
-            guard: 'back at the start',
-            target: 'Finished',
+        Advancing: {
+          entry: ['pickNextCell', 'pushToStack'],
+          after: {
+            SEEK_INTERVAL: {
+              guard: 'playing is allowed',
+              target: 'Seeking',
+            },
           },
-          {
-            target: 'Seeking',
+          always: {
+            guard: 'reached a dead end',
+            target: 'Backtracking',
           },
-        ],
-      },
-      Finished: {
-        entry: sendParent({ type: 'generation.finish' }),
+        },
+        Backtracking: {
+          entry: 'popFromStack',
+          always: [
+            {
+              guard: 'back at the start',
+              target: 'Finished',
+            },
+            {
+              target: 'Seeking',
+            },
+          ],
+        },
+        Finished: {
+          entry: sendParent({ type: 'generation.finish' }),
+        },
       },
     },
     {
       guards: {
-        'playing is allowed': (ctx) => ctx.canPlay,
-        'reached a dead end': ({ eligibleNeighbors }) =>
+        'playing is allowed': ({ context }) => context.canPlay,
+        'reached a dead end': ({ context: { eligibleNeighbors } }) =>
           eligibleNeighbors.length === 0,
-        'back at the start': ({ stack }) => stack.length === 0,
+        'back at the start': ({ context: { stack } }) => stack.length === 0,
       },
       actions: {
         initGeneration: assign({
-          currentCell: (ctx) => (ctx.grid as IGrid).getStartCell(),
+          currentCell: ({ context }) => (context.grid as IGrid).getStartCell(),
         }),
-        visitStartCell: (ctx) => {
-          const currentCell = (ctx.grid as IGrid).getStartCell();
-          return currentCell.visit(null, ctx.pathId);
+        visitStartCell: ({ context }) => {
+          const currentCell = (context.grid as IGrid).getStartCell();
+          return currentCell.visit(null, context.pathId);
         },
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        play: assign({ canPlay: (_) => true }),
+        // play: assign({ canPlay: true }),
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        pause: assign({ canPlay: (_) => false }),
+        // pause: assign({ canPlay: false }),
         findNeighbors: assign({
-          eligibleNeighbors: ({ grid, currentCell }) =>
-            (grid as IGrid).getEligibleNeighbors(currentCell as ICell),
+          eligibleNeighbors: ({ context }) => {
+            const { grid, currentCell } = context;
+            return (grid as IGrid).getEligibleNeighbors(currentCell as ICell);
+          },
         }),
-        pickNextCell: assign(({ grid, pathId, startIndex, currentCell }) => {
-          const verifiedGrid = grid as IGrid;
-          return {
-            currentCell: seek({
-              grid: verifiedGrid,
-              pathId,
-              current: currentCell as ICell,
-              startIndex,
-            }),
-          };
-        }),
-        pushToStack: assign(({ stack, currentCell }) => {
+        pickNextCell: assign(
+          ({ context: { grid, pathId, startIndex, currentCell } }) => {
+            const verifiedGrid = grid as IGrid;
+            return {
+              currentCell: seek({
+                grid: verifiedGrid,
+                pathId,
+                current: currentCell as ICell,
+                startIndex,
+              }),
+            };
+          }
+        ),
+        pushToStack: assign(({ context: { stack, currentCell } }) => {
           if (currentCell) {
             stack.push(currentCell);
           }
           return { stack };
         }),
-        popFromStack: assign(({ stack }) => {
+        popFromStack: assign(({ context: { stack } }) => {
           const prevCell = stack.pop() as ICell;
           prevCell?.setAsBacktrack();
           return { stack, currentCell: prevCell };
         }),
       },
       delays: {
-        SEEK_INTERVAL: ({ fps }) => 1000 / fps,
+        SEEK_INTERVAL: ({ context: { fps } }) => 1000 / fps,
       },
-      'controls.step.forward': {
-        target: '.Seeking',
-      },
-    },
-  }).provide({
-    guards: {
-      'playing is allowed': ({ context }) => context.canPlay,
-      'reached a dead end': ({ context }) =>
-        context.eligibleNeighbors.length === 0,
-      'back at the start': ({ context }) => context.stack.length === 0,
-    },
-    actions: {
-      initGeneration: assign({
-        currentCell: ({ context }) => (context.grid as IGrid).getStartCell(),
-      }),
-      visitStartCell: ({ context }) => {
-        const currentCell = (context.grid as IGrid).getStartCell();
-        return currentCell.visit(null, context.pathId);
-      },
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      play: assign({ canPlay: true }),
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      pause: assign({ canPlay: false }),
-      findNeighbors: assign({
-        eligibleNeighbors: ({ context: { grid, currentCell } }) =>
-          (grid as IGrid).getEligibleNeighbors(currentCell as ICell),
-      }),
-      pickNextCell: assign(
-        ({ context: { grid, pathId, startIndex, currentCell } }) => {
-          const verifiedGrid = grid as IGrid;
-          return {
-            currentCell: seek({
-              grid: verifiedGrid,
-              pathId,
-              current: currentCell as ICell,
-              startIndex,
-            }),
-          };
-        }
-      ),
-      pushToStack: assign(({ context: { stack, currentCell } }) => {
-        if (currentCell) {
-          stack.push(currentCell);
-        }
-        return { stack };
-      }),
-      popFromStack: assign(({ context: { stack } }) => {
-        const prevCell = stack.pop() as ICell;
-        prevCell?.setAsBacktrack();
-        return { stack, currentCell: prevCell };
-      }),
-    },
-    delays: {
-      SEEK_INTERVAL: ({ context: { fps } }) => 1000 / fps,
-    },
-  });
+    }
+  );
