0708b06a0649bacf6f037dd08480a871b5f97a99
Kevin Maes <kevin@kevinmaes.com>
2023-09-09 11:22:10 +0200

    Separate AlgorithmGenerationParams from cosmetic params

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,4 @@
-export interface GenerationParams {
-  // Needed only by the State/Grid/Cells (not the algorothm).
-  borderWeight: number; // Passed down to Grid/cell.
-
-  // Needed only by the State/Grid/Cells (not the algorothm).
-  cellSize: number; // Passed down to Grid/cell.
-
+interface AlgorithmGenerationParams {
   // Maybe controlled by the App but passed down to the
   // algo machine so it can set up its own timer
   // but still be overridden by play/pause functionality.
@@ -15,4 +9,12 @@ export interface GenerationParams {
 
   // Needed only by the State/Grid/Cells (not the algorothm).
   gridRows: number; // Passed down to algo.
+}
+
+export interface GenerationParams extends AlgorithmGenerationParams {
+  // Needed only by the State/Grid/Cells (not the algorothm).
+  borderWeight: number; // Passed down to Grid/cell.
+
+  // Needed only by the State/Grid/Cells (not the algorothm).
+  cellSize: number; // Passed down to Grid/cell.
 }
