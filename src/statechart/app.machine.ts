aee3b03ec42210bfe3b2a3a4d5410d6f4d0b7e7f
Kevin Maes <kevin@kevinmaes.com>
2023-09-09 10:56:53 +0200

    Move defaultGenerationParams into context in machine config

--- src/statechart/app.machine.ts
+++ src/statechart/app.machine.ts
@@ -12,14 +12,6 @@ const CellSize = {
   MAX: 25,
 };
 
-export const defaultGenerationParams: GenerationParams = {
-  borderWeight: BORDER_WEIGHT_DEFAULT,
-  cellSize: CellSize.DEFAULT,
-  fps: FPS_DEFAULT,
-  gridColumns: GRID_SIZE_DEFAULT,
-  gridRows: GRID_SIZE_DEFAULT,
-};
-
 export const appMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QEMAOqDEMB2YBOyALgJYD22AdKsgQLYWxiEDaADALqKiqmzEnkuIAB6IAtAE4ATBIoSAHAGZWU+QDYA7Gplr5AGhABPcQBYJrCgEZ50xRICsa1pZP2HAX3cG0qCgEkIABswDDwwADNYCmJsACswAGMWDiEePgFsIVEEMQ0TCykTeVZFJzc1ZSkDYxzFe0sKVXtFE1VLR3qNe09vdAoAcTBcAhJsKAwIYlhUQORDCgBXVAgiMDZOJBA0-jJMzez5KQ1G+w1WIqk61i7q8Qk1CkUtdQlS+XlLKXsTHpAfAaG+CIMXGOCBGQo4RiUwAFutUrwdoJ9ognIoKHlFJ9LPd6vl7LccporEoZE8ZBJ2q1fv9BsNgWN-NgdshAsQAF4gjAJciEPCkQJRGZzeGbbYZLLiRSFRrqLR1NSWVgVEyEsSXCzKLquSylLTNGl9OngkEUAAKs0MXJ52D5AqFyAWjFF3EREpRCEu8goNnaxSelicrHs+iM4i+snaCg+wY0l3aikNvmNI1NFrm1t5-MFDEIpFQLq2bt2kpyGnRNg0SnMEar5bVLhMFCKTg+Wle10OSYB9NGUHNjsYEG5WftVEthfFJY9hSbrW+dm0rkqGjVdQaTRabQ6li63ZTDP7ZsHkBHtuzUVgeYLKTFxeRoGyuQrEirdhUbjrijVJge7UDgalLu7wSD8Xh-EagKpoyx5OqeNp2jmV5gL44SkHgADuNAQJO957I+qK-o8oHKtolzSholhrnYjRPNIyrSI40rdOB-wACLkCE-xhFeNDJBsrrpNOBE5K8Dzlsoai6JcUhqA4hLtM2ujyGcaghsUnSJr82CkBAcBCD4CJCQ+Ih3PITYSaRhzSnJBJhjkbhSI0JjtLojgVLo3YBMERlIvhpk5OoFifnUNi6OcZhrqojxYmiahmHYUj7lBh6+e6IliM03pKqcXRYuZSirvZmXnBiFSfBUXTmGYajJb2pp+MyJCshyIJpcJAViGYFg5RoeXWEUpRqhqjzXN89R6mpWm9MmKV9ualptXexn+U+nzopYLh9dKyisMURzDc0GISNIr5aJ8fUqHVJowSeEDtSZ2SOLI5UOM4qiycNpSNMqLjWOYr67rVrF9BxuAPatqL2D1hxxlIUiBqdVT2Yo3qtLJrAKF8ypmGBnhAA */
   createMachine(
@@ -50,7 +42,13 @@ export const appMachine =
       },
       context: {
         mazeId: '',
-        generationParams: defaultGenerationParams,
+        generationParams: {
+          borderWeight: BORDER_WEIGHT_DEFAULT,
+          cellSize: CellSize.DEFAULT,
+          fps: FPS_DEFAULT,
+          gridColumns: GRID_SIZE_DEFAULT,
+          gridRows: GRID_SIZE_DEFAULT,
+        },
         gridRef: undefined,
         generationSessionId: new Date().getTime(),
       },
