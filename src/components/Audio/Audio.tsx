86c1ded6d44793e136bafbb63673e656a933a917
Kevin Maes <kevin@kevinmaes.com>
2023-09-11 21:54:32 +0200

    Separate audioOptions to its own file

--- src/components/Audio/Audio.tsx
+++ src/components/Audio/Audio.tsx
@@ -3,28 +3,7 @@ import useSound from 'use-sound';
 import { frequencies, diatonicScales } from './notes';
 import { ActorRefFrom } from 'xstate';
 import { generationAlgorithmMachine } from '../../statechart/recursiveBacktracker.machine';
-
-const audioOptions: Array<{
-  name: string;
-  startFrequency: keyof typeof frequencies;
-  path: string;
-}> = [
-  {
-    name: 'Marimba 1',
-    startFrequency: 'C3',
-    path: '/sounds/Ensoniq-ESQ-1-Marimba-C3.wav',
-  },
-  {
-    name: 'Marimba 2',
-    startFrequency: 'C5',
-    path: '/sounds/marimba-c5.wav',
-  },
-  {
-    name: 'Synth 1',
-    startFrequency: 'C1',
-    path: '/sounds/Casio-CZ-5000-Synth-Bass-C1.wav',
-  },
-];
+import { audioOptions } from './audioOptions';
 
 interface Props {
   algorithmActor: ActorRefFrom<typeof generationAlgorithmMachine>;
