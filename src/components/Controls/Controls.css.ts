f233e2611d6472ef8e150b061af27ee43cccbdb3
Kevin Maes <kevin@kevinmaes.com>
2023-09-16 20:00:44 +0200

    Delete unused play controls

--- src/components/Controls/Controls.css.ts
+++ src/components/Controls/Controls.css.ts
@@ -1,12 +1,5 @@
 import styled, { css, keyframes } from 'styled-components';
 
-export const StartOver = styled.button``;
-export const Play = styled.button``;
-export const Stop = styled.button``;
-export const Pause = styled.button``;
-export const StepBack = styled.button``;
-export const StepForward = styled.button``;
-
 export const ControlsContainer = styled.div`
   width: 170px;
   height: 50px;
