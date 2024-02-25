ec2a565682780f8dabaf458a5da05ccc658851db
Kevin Maes <kevin@kevinmaes.com>
2024-02-25 19:05:38 +0100

    Use imported AlgorithmGenerationParams in case more are added

--- src/statechart/recursiveBacktracker.machine.ts
+++ src/statechart/recursiveBacktracker.machine.ts
@@ -2,6 +2,7 @@ import { assign, setup } from 'xstate';
 import type { IGrid } from '../components/generation/Grid';
 import { ICell } from '../components/generation/Cell';
 import { seek } from '../components/generation/seek';
+import { AlgorithmGenerationParams } from '../types';
 
 export const generationAlgorithmMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QwHZgE4EMAuBLA9igIIA2U+6u2AFgLYCymAxtbmgMROHbr4mwA6AA4lMATwDaABgC6iUEPywqBFPJAAPRAEYArAN0AmAGwBOUwHYAHABYLxm8YDMV3QBoQYxAFpthmwJWFoZWLrqW4bp6AL7RHqgYOKqk5JQ0DMysHFwoPHyCQpgArrBg0nJIIIrKeITqWggmhoE2VlbaLi7aUqYeXgg2dgJONrpW-iMuFoOx8WBoWLXEZBRUdIwsbGCc3Lz8ArDYYEICAGYUAO6Y6BDl6tUqdZUNZoEWTqbGQWNOTg7unh82k+BhsUlaNmBthMul0sxACUWyRWaXWmS2AgA4vNEksAAQASQgJG2iKShAO2Gu2DulQeS3qPjsTgMxmCejso0Mej6PmcBn8IWcxn83V+xnhZKWKVW6Q2WTAAgJKBUmBIuAAXmwoOwNIccIrMKcjugABQAZQAopaANIAfQJADkACqWgBKADUiAAZACU7ClyNSawymzQSpVeDVmu1tIUSkeameQOmgScUmMukcUlC0ykTl5CH5zlslisxnzUjFkpxSMIMtRoYVAnNYDAAGttew41UEwzkwh01IWm0bKZBsDdFXjIX7MNgcZtBZ7FObE4awtyctg3L0eGiBAAG6YFBMLs9+mqRlF9oCbmmfMriuw7Sz4wCZyGblftpswzmDdcSDWU0TDRUD2PU8uz1KkjgEI0TQta17SdV1PR9f1A3rFEQ3lDEIJPM8UCgC8+yvAduSsARtFsZdBUGKxxxsQtvF+QIpHsDNwSXaZwkAuttxApsMQAIWYdseHE89ZHuMinlABpTG0AIlzMcYLG6NpgkLbQHA-IxjC+Rdgj+dc4gRWstwbXC90VMSmAkrAHPPbQKnjGpyIUxBrBZX5-HLLM9GU5jAUaQxh0M8IOK-JceiMWJzJQfAIDgdQsMExs8LQWSPPkzQfCzYddJMbjwnecwWJCFloVcQwjB6OrTDMuZN2lHDdzArFLPxIkSRyxNr28RwqJ6QznFMKrx1fULfDXD82TK0wol0iwxn4qz2tA5tlVVdUtWI-r+y8hAhom6iRQrSEyo+XpQrq4Y10+Cb7GcSF7HWtqdy2jFWw7bVDs8-KEAsCKgmsbplMncZCxcYYnBexi-CnD4bA+4DMtsgQCKgg66TkpNjpsblQVGVaXGZSYYao34EaUur83HNHsK+4Tw3sxypNx9yBoHcth3aUJv3edoXELKI4fCPwTBGKRZdMJmMpszqADE2FwWBqEgAG8oaIJh10EZl3zctOisQsQn0eH0zHYJ3lhCUEqAA */
@@ -15,9 +16,7 @@ export const generationAlgorithmMachine =
         pathId: string;
         stack: ICell[];
         startIndex: number;
-      } & {
-        fps: number;
-      };
+      } & AlgorithmGenerationParams;
       input: {
         canPlay: boolean;
         fps: number;
